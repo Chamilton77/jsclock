ad0827afbb70b596ea997e5f65c44f233cdcabdc
Carson Hamilton <cjcham77@gmail.com>
2016-11-04 15:05:53 -0600

    my clock assignment

---
+++ clock.js
@@ -0,0 +1,25 @@
+//This displays the current time by getting it out of the date object in javascript.
+var d, h, m, s, color;
+function displayTime() {
+	d = new Date(); 
+	h = d.getHours();
+	m = d.getMinutes();
+	s = d.getSeconds();
+	
+	//add zero to the left of the numbers if they are single digits
+	if(h <= 9) h = '0'+h;
+	if(m <= 9) m = '0'+m;
+	if(s <= 9) s = '0'+s;
+	
+	color = "#"+h+m+s;
+	//set background color
+	document.body.style.background = color;
+	//set time
+	document.getElementById("clock").innerHTML = color;
+	
+	//retrigger the function every second
+	setTimeout(displayTime, 1000);
+}
+
+//call the function
+displayTime();
